e61894b00fe7a2fac877cf0eb7be9d29301ad4c9
tuhindewan <tuhinsshadow@gmail.com>
2018-09-15 00:04:09 -0800

    api resource controller

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::apiResource('/products','ProductController');
+
+Route::prefix('/products')->group(function () {
+    Route::apiResource('/{product}/reviews','ReviewController');
+});
